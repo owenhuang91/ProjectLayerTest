00a30273cc0df8727d74929c89b0ad3e8b32b51a
Owen <owen2862@live.com>
2017-07-16 16:12:14 +0800

    新增MVC網站 並預設好一般寫法

--- ProjectLayerTest/Controllers/CategoryController.cs
+++ ProjectLayerTest/Controllers/CategoryController.cs
@@ -7,7 +7,7 @@ using ProjectLayerTest.Models;
 
 namespace ProjectLayerTest.Controllers
 {
-    public class HomeController : Controller
+    public class CategoryController : Controller
     {
         public ActionResult Index()
         {
@@ -15,22 +15,8 @@ namespace ProjectLayerTest.Controllers
             {
                 var query = db.Categories.OrderBy(x => x.CategoryID);
                 ViewData.Model = query.ToList();
+                return View();
             }
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
         }
     }
 }
---
+++ ProjectLayerTest/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectLayerTest.Models;
+
+namespace ProjectLayerTest.Controllers
+{
+    public class ProductController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        public ActionResult Index()
+        {
+            var products = db.Products.OrderByDescending(x => x.ProductID);
+            ViewData.Model = products.ToList();
+            return View();
+        }
+    }
+}
